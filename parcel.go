ac19da54d04f7c8f51b373825055830e49b515ea
Maria Petrova <maria-petrova1296@yandex.ru>
2024-02-22 01:12:27 +0300

    Описаны функции Add и Get, настроено подключение к БД

--- parcel.go
+++ parcel.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 type ParcelStore struct {
@@ -14,18 +15,36 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
-
+	response, err := s.db.Exec("INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, :address, :created_at)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+	if err != nil {
+		fmt.Printf("Add s.db.Exec error %v \n", err)
+		return 0, err
+	}
+	lastId, err := response.LastInsertId()
+	if err != nil {
+		fmt.Printf("Add response.LastInsertId error %v \n", err)
+		return 0, err
+	}
 	// верните идентификатор последней добавленной записи
-	return 0, nil
+	return int(lastId), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
 	// реализуйте чтение строки по заданному number
 	// здесь из таблицы должна вернуться только одна строка
-
+	row := s.db.QueryRow("SELECT client, status, address, created_at FROM parcel WHERE id = :id",
+		sql.Named("id", number))
 	// заполните объект Parcel данными из таблицы
 	p := Parcel{}
-
+	err := row.Scan(&p.Client, &p.Status, &p.Address, &p.CreatedAt)
+	if err != nil {
+		fmt.Printf("Get row.Scan error %v \n", err)
+		return p, err
+	}
 	return p, nil
 }
 
